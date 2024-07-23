5cf03e8ca175979383c5d598e49b083442b6c1ee
ZahraaNalbanPharmaciaty <zahraa.nalban@pharmaciaty.com>
2024-07-23 10:22:39 +0400

    changes made to model

--- src/models/ticketHistory.py
+++ src/models/ticketHistory.py
@@ -1,11 +1,13 @@
-from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String, Boolean
 from src.utils.database import Base
 from sqlalchemy.sql import func
 
 class TicketHistory(Base):
     __tablename__ = "ticketHistory"
     id = Column(Integer, primary_key=True, index=True)
-    ticket_id = Column(Integer, nullable=False)
+    ticket_id = Column(Integer, ForeignKey("tickets.id"), nullable=False)
     created_at = Column(DateTime, default=func.now())
     user_id = Column(Integer, nullable=False)
     username = Column(String, nullable=False)
+
+    
