caf0d645cdbc823b721c263bc693f0527be65125
ZahraaNalbanPharmaciaty <zahraa.nalban@pharmaciaty.com>
2024-07-23 14:52:56 +0400

    get ticket history

--- src/api/ticket.py
+++ src/api/ticket.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from src.controllers.ticket import create_ticket, get_all_tickets, update_ticket_status
-from src.schemas.ticket import TicketCreate, TicketDetails, TicketOrder, TicketUpdate
+from src.controllers.ticket import create_ticket, get_ticket_history, get_tickets, update_ticket_status
+from src.schemas.ticket import TicketCreate, TicketDetails, TicketOrder
 
 from src.models.user import User
 from src.utils.database import get_db
@@ -10,6 +10,27 @@ from src.utils.auth import RoleChecker
 from src.schemas.ticket_status import TicketStatus
 
 tickets = APIRouter(prefix="/tickets", tags=["Tickets"])
+
+
+@tickets.get("/", response_model=list[TicketDetails])
+async def ticket_get(
+    user_id: str | None = None,
+    status: TicketStatus | None = None,
+    skip: int = 0,
+    limit: int = 10,
+    order: TicketOrder = TicketOrder.LAT,
+    db: Session = Depends(get_db),
+    user: User = Depends(RoleChecker(allowed_roles=["Pharmacist", "Customer"])),
+):
+    return get_tickets(
+        db=db,
+        user_id=user_id,
+        user=user,
+        status=status,
+        skip=skip,
+        limit=limit,
+        order=order,
+    )
 
 
 @tickets.post("/", response_model=TicketDetails)
@@ -21,39 +42,18 @@ async def ticket_create(
     return create_ticket(ticket=ticket, user=user, db=db)
 
 
-# @tickets.get("/", response_model=list[TicketDetails])
-# async def ticket_get_my(
-#     status: TicketStatus | None = None,
-#     skip: int = 0,
-#     limit: int = 10,
-#     order: TicketOrder = TicketOrder.LAT,
-#     db: Session = Depends(get_db),
-#     user: User = Depends(RoleChecker(allowed_roles=["Customer"])),
-# ):
-#     return get_user_tickets(
-#         db=db, user=user, status=status, skip=skip, limit=limit, order=order
-#     )
-
-
-@tickets.get("/all", response_model=list[TicketDetails])
-async def ticket_get_all(
-    user_id: str | None = None,
-    status: TicketStatus | None = None,
-    skip: int = 0,
-    limit: int = 10,
-    order: TicketOrder = TicketOrder.LAT,
-    db: Session = Depends(get_db),
-    user: User = Depends(RoleChecker(allowed_roles=["Pharmacist", "Customer"]))
-):
-    return get_all_tickets(
-        db=db, user_id=user_id, user=user, status=status, skip=skip, limit=limit, order=order
-    )
-
 @tickets.put("/", response_model=TicketDetails)
 async def ticket_status_update(
     ticket_id: int,
-    status: TicketStatus | None = None,
+    status: TicketStatus,
     db: Session = Depends(get_db),
-    _: User = Depends(RoleChecker(allowed_roles=["Customer"]))
+    _: User = Depends(RoleChecker(allowed_roles=["Pharmacist"])),
 ):
     return update_ticket_status(status=status, ticket_id=ticket_id, db=db)
+
+@tickets.history("/history", response_model = TicketDetails)
+async def ticket_history(
+    ticket_id: int,
+    db: Session = Depends(get_db)
+):
+    return get_ticket_history()
