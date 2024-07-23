ba8871380ce269d273cef38d60658f90e9bf95b4
Amaan Al Mir <amaan.mir@pharmaciaty.com>
2024-07-23 11:56:20 +0400

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from src.utils.database import init_db
-from src.api import auth, user, ticket_notes
+from src.api import auth, user, ticket, ticket_notes
 
 
 def my_app():
@@ -13,6 +13,7 @@ def my_app():
     app = FastAPI(title=os.getenv("TITLE"))
     app.include_router(auth.auth)
     app.include_router(user.users)
+    app.include_router(ticket.tickets)
     app.include_router(ticket_notes.ticket_notes)
 
     return app
